8507063878c9dc648339275571e3c36e656d1075
stephen0771 <stephenkyalo437@gmail.com>
2026-03-17 21:02:37 +0300

    initial api

--- ToDo  App/ToDo  App.py
+++ ToDo  App/ToDo  App.py
@@ -1,39 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-ToDoApp =__name__
-app = Flask(ToDoApp)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'SQLITE:///todo.db'
-db = SQLAlchemy(app)
-class ToDo(db.Model):
-    id = db.column(db.integer,primary_key=True)
-    title = db.column(db.string(100), nullable=False)
-    description = db.column(db.string(200), nullable=False)
-    def __repr__(self):
-        return f'<ToDo {self.id}>'
-    @app.route('/')
-    def index():
-        return jsonify({
-            "message":"welcome to simple ToDo List API",
-            "status":"online",
-            "version":"1.0.0",
-            "created_by":"s.k mulwa",
-            "inspected_by":"Slyvester Musyoki"
-        })
-        todos = ToDo.query.all()
-        return render_template('index.html', todos=todos)
-    """@app.route('/add', methods=['POST'])
-    def add():
-        title = request.form['title']
-        description = request.form['description']
-        new_todo = ToDo(title=title, description=description)
-        db.session.add(new_todo)
-        db.session.commit()
-        return redirect(url_for())"""
-    @app.route('/delete/<int:id>')
-    def delete(id):
-        ToDo.query.filter_by(id=id).delete()
-        db.session.commit()
-        return redirect(url_for('index'))
+from flask import Flask,jsonify
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return jsonify({
+        "message":"welcome to simple ToDo List API",
+        "status":"online",
+        "version":"1.0.0",
+        "created_by":"s.k mulwa",
+        "inspected_by":"Slyvester Musyoki"
+    })
 if __name__ == '__ main_':
     db.create_all()
     app.run(debug=True)
